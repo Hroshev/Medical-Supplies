9eb5a70733dd809321e420822846a3dc7f82516a
Ivan Hroshev <you@example.com>
2021-11-16 00:19:55 +0200

    Finished work

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,8 @@
 (function (){
     let burger = document.querySelector('.header__burger');
+    let nav = document.querySelector('.header__nav');
     burger.addEventListener('click', () => {
     burger.classList.toggle('header__active');
+    nav.classList.toggle('burger__active');
 });
 }());
